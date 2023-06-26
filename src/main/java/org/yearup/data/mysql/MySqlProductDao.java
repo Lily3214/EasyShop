9d658b27b457abdf213e88b0d58489c28ed245f7
Lily3214 <yeus1995@gmail.com>
2023-06-26 16:11:27 -0400

    phase 2 bug1

--- src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -26,31 +26,40 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
     public List<Product> search(Integer categoryId, BigDecimal minPrice, BigDecimal maxPrice, String color)
     {
         List<Product> products = new ArrayList<>();
-
+        // string concatnation
         String sql = "SELECT * FROM products " +
-                "WHERE (category_id = ? OR ? IS NULL) " +
+                "WHERE (category_id = ? OR ? = -1) " +
                 "   AND (price >= ? OR ? IS NULL) " +
                 "   AND (price <= ? OR ? IS NULL) " +
-                "   AND (color = ? OR ? IS NULL) ";
+                "   AND (color = ? OR ? = '') ";
 
-        try (Connection connection = getConnection()) {
+        categoryId = categoryId == null ? -1 : categoryId;
+        minPrice = minPrice == null ? new BigDecimal("-1") : minPrice;
+        maxPrice = maxPrice == null ? new BigDecimal("-1") : maxPrice;
+        color = color == null ? "" : color;
+
+        try (Connection connection = getConnection())
+        {
             PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setObject(1, categoryId);
-            statement.setObject(2, categoryId);
-            statement.setObject(3, minPrice);
-            statement.setObject(4, minPrice);
-            statement.setObject(5, maxPrice);
-            statement.setObject(6, maxPrice);
-            statement.setObject(7, color);
-            statement.setObject(8, color);
+            statement.setInt(1, categoryId);
+            statement.setInt(2, categoryId);
+            statement.setBigDecimal(3, minPrice);
+            statement.setBigDecimal(4, minPrice);
+            statement.setBigDecimal(5, maxPrice);
+            statement.setBigDecimal(6, maxPrice);
+            statement.setString(7, color);
+            statement.setString(8, color);
 
             ResultSet row = statement.executeQuery();
 
-            while (row.next()) {
+            while (row.next())
+            {
                 Product product = mapRow(row);
                 products.add(product);
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             throw new RuntimeException(e);
         }
 
