e9071786415a7f4e9c10868b2b596a1389a546cd
Lily3214 <yeus1995@gmail.com>
2023-06-27 10:01:30 -0400

    Phase 2 Bug1

--- src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -29,8 +29,8 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
         // string concatnation
         String sql = "SELECT * FROM products " +
                 "WHERE (category_id = ? OR ? = -1) " +
-                "   AND (price >= ? OR ? -1) " +
-                "   AND (price <= ? OR ? -1) " +
+                "   AND (price >= ? OR ? = -1) " +
+                "   AND (price <= ? OR ? = -1) " +
                 "   AND (color = ? OR ? = '') ";
 
         categoryId = categoryId == null ? -1 : categoryId;
